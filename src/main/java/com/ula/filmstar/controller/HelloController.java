9d95d55a8f5a17cb765eb0fdd1535845611ccf6f
ukowalczyk <ukowalczyk@ukowalczyk-gda.pgs-soft.com>
2017-03-03 09:33:11 +0100

    log4j configured

--- src/main/java/com/ula/filmstar/controller/HelloController.java
+++ src/main/java/com/ula/filmstar/controller/HelloController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.ula.filmstar.model.Film;
 import com.ula.filmstar.repository.FilmRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class HelloController {
 
 	@Autowired
@@ -17,6 +20,7 @@ public class HelloController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String printHello(ModelMap model) {
+		log.info("Logi dzialaja");
 		Film film = new Film("tytul", "opis");
 		filmRepository.save(film);
 		model.addAttribute("message", "Hello Spring MVC Framework!");
